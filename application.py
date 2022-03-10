a8b5a6cb4c1fef34b75fdde1e1a43e9d81073fc7
hulica <linczer.andrea@gmail.com>
2022-03-10 19:04:57 +0100

    Greg changed permanent sessions to true

--- application.py
+++ application.py
@@ -36,7 +36,7 @@ def after_request(response):
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 
 Session(app)
@@ -139,7 +139,7 @@ def login():
     """Log user in"""
 
     # Forget any user_id
-    session.clear()
+    # session.clear()
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
